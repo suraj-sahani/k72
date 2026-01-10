cc47f8761c37b45a00402b87df26802424fa91aa
suraj-sahani <suraj.sahani.961@gmail.com>
2026-01-10 08:16:08 +0530

    feat(routing): Add react router and setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./routes/home";
+import Agents from "./routes/agents";
+import Projects from "./routes/projects";
 
 function App() {
   return (
-    <>
-      <div className="h-screen w-full bg-gray-300 text-4xl flex items-center justify-center">
-        Hello
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/agents" element={<Agents />} />
+      <Route path="/projects" element={<Projects />} />
+    </Routes>
   );
 }
 
---
+++ src/routes/agents.tsx
@@ -0,0 +1,7 @@
+export default function Agents() {
+  return (
+    <div>
+      <h1>Agents</h1>
+    </div>
+  );
+}
